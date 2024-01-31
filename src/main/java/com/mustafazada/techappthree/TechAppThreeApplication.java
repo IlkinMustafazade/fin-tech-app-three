8983265e0310aeabfb44042b20e2fbf4c6090097
Ilkin <ilkinmustafazadeh@gmail.com>
2024-01-31 20:08:18 +0400

    first commit

---
+++ src/main/java/com/mustafazada/techappthree/TechAppThreeApplication.java
@@ -0,0 +1,13 @@
+package com.mustafazada.techappthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAppThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechAppThreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustafazada/techappthree/TechAppThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafazada.techappthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAppThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
